73e581e4a08b19eadd6cc87f52bbf67a6424b810
Pablo Jacobi Inostroza <nettox@gmail.com>
2019-02-28 11:20:48 -0300

    Updated mailer sender

--- dist/mailer.php
+++ dist/mailer.php
@@ -16,11 +16,11 @@
       $mail->SMTPAuth = true;
       //Si activas esto se mostrarán todos los logs de errores
       //$mail->SMTPDebug = 2;
-      $from = "pablo@pyro.cl"; // Correo desde el cual se mandan las solicitudes
+      $from = "sender@macgarage.cl"; // Correo desde el cual se mandan las solicitudes
       $mail->SMTPSecure = 'tls';
       $mail->Host = "smtp.gmail.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
       $mail->Username = $from; // Correo completo a utilizar
-      $mail->Password = "canela00"; // Contraseña
+      $mail->Password = "Sender$2019.,"; // Contraseña
       $mail->Port = 587; // Puerto a utilizar
       $mail->AddReplyTo($_POST['email'], $_POST['name']);
       $mail->SetFrom($from, $_POST['name'], $_POST['name']);
